248f07653f3081cef61809f0038ec528841f0d05
Sunnylin0320 <sunny8503201@gmail.com>
2023-06-25 14:27:14 +0800

    feat 搜尋功能

--- app.js
+++ app.js
@@ -15,11 +15,20 @@ app.get("/", (req, res) => {
   res.render("index", { restaurants: restaurantList.results });
 });
 
+
 app.get("/restaurants/:restaurant_id", (req, res) => {
   const restaurant = restaurantList.results.find(
     (restaurant) => restaurant.id.toString() === req.params.restaurant_id
   );
   res.render("show", { restaurant: restaurant });
+});
+
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase());
+  });
+  res.render("index", { restaurants: restaurants, keyword: keyword });
 });
 
 
