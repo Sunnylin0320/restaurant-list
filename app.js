d03035781972ade30c67e7e192ad63804410e0d7
Sunnylin0320 <sunny8503201@gmail.com>
2023-06-25 12:40:04 +0800

    feat: install expree-handlebars

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const exphbs = require("express-handlebars");
+
 // routes setting
 app.get("/", (req, res) => {
-  res.send("This is my restaurant list built with Express");
+  res.render("index");
 });
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // start and listen on the Express server
 app.listen(port, () => {
