a002fdc65f0882342bf55a605e58574b92c98c32
Sunnylin0320 <sunny8503201@gmail.com>
2023-06-28 13:08:08 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 const port = 3000;
 
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 const Restaurant = require("./models/restaurant");
 const restaurantList = require("./restaurant.json");
 
@@ -62,6 +63,13 @@ app.get("/search", (req, res) => {
   res.render("index", { restaurants: restaurants, keyword: keyword });
 });
 
+app.post("/restaurants", (req, res) => {
+  const name = req.body.name; // 從 req.body 拿出表單裡的 name 資料
+  return Restaurant.create({ name }) // 存入資料庫
+    .then(() => res.redirect("/")) // 新增完成後導回首頁
+    .catch((error) => console.log(error));
+});
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
