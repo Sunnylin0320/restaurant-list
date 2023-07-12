cd882e6635338339a9bba863d941c1785220fbfa
Sunnylin0320 <sunny8503201@gmail.com>
2023-07-12 18:38:29 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const app = express();
 const methodOverride = require("method-override");
@@ -19,6 +20,14 @@ require("./config/mongoose");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 //提供靜態檔案
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
