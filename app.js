c8a1766443983b43ed4a764f295b2226d73b47a4
Sunnylin0320 <sunny8503201@gmail.com>
2023-07-14 11:09:50 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const app = express();
 const methodOverride = require("method-override");
+const flash = require("connect-flash");
 const bodyParser = require("body-parser");
 const handlebars = require("handlebars");
 const seed = require("./restaurant.json");
@@ -36,13 +37,21 @@ app.use(methodOverride("_method"));
 
 
 
+
+
+usePassport(app);
+
+
+app.use(flash());
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.warning_msg = req.flash("warning_msg");
   next();
 });
 
-usePassport(app);
 app.use(routes);
 
 app.listen(3000, () => {
